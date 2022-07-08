b94c9b7941b07650f34fe92dfe1fdaf35e13041f
Riley Martin <riley.willow@gmail.com>
2022-07-08 11:26:08 -0500

    Create modular file structure

---
+++ src/App/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      groceryListItems: []
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Grocery List</h1>
+      </>
+    )
+  }
+}
+
+export default App
